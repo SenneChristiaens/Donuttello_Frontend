b5c5c43420452bd722626d95a22ec809aa296b44
Senne <>
2022-11-28 21:50:07 +0100

    fix: styling footer & imports in index

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -3,7 +3,7 @@
 <template>
   <footer class="footer">
     <img class="footer__img" src="/src/assets/logo.png" alt="logo" />
-    <router-link to="/"><a class="nav__item">Home</a></router-link>
+      <router-link to="/"><a class="nav__item">Home</a></router-link>
       <router-link to="/configurator"><a class="nav__item">Configurator</a></router-link>
       <router-link to="/gallery"><a class="nav__item">Gallerij</a></router-link>
     <p class="footer__p">© 2022 reserved by Best Donuts gcv</p>
