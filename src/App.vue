90dbbc9d753a4d91c4cc44883d38f2692dcbc656
Senne <>
2022-11-28 19:30:38 +0100

    feat: add donuts from api to Gallery.vue

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,11 @@
 <script setup>
-
+import Gallery from './components/Gallery.vue';
 </script>
 
 <template>
-
+    <div id="app">
+        <Gallery />
+    </div>
 </template>
 
 <style></style>
--- src/components/Gallery.vue
+++ src/components/Gallery.vue
@@ -1,11 +1,36 @@
 <script setup>
-  
+import { reactive, onMounted } from "vue";
+import moment from "moment";
+import Footer from "./Footer.vue";
+import Navigation from "./Navigation.vue";
+
+let donuts = reactive({ donuts: [] });
+
+onMounted(() => {
+  let apiUrl = "https://donuttello-backend-5chz.onrender.com/api/v1/donuts";
+  fetch(apiUrl)
+    .then((response) => response.json())
+    .then((data) => {
+      donuts.donuts = data.data.donuts;
+      donuts.donuts.forEach((donut) => {
+        donut.date = moment(donut.date).format("DD MMMM YYYY");
+      });
+    });
+});
 </script>
 
 <template>
+  <Navigation />
+  <h1>Gallery</h1>
 
+  <div class="gallery">
+    <div class="gallery__item" v-for="donut in donuts.donuts">
+      <h2 class="gallery__title">{{ donut.name }}</h2>
+      <p class="gallery__date">{{ donut.date }}</p>
+      <p class="gallery__votes">Votes: {{ donut.votes }}</p>
+    </div>
+  </div>
+  <Footer />
 </template>
 
-<style>
-
-</style>
+<style></style>
