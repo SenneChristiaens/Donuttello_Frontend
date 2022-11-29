674a15835b5a237c62818b1e985e744dc4f1831a
Senne <>
2022-11-29 17:07:11 +0100

    feat: create form on configurator page & add style

--- src/views/Configurator.vue
+++ src/views/Configurator.vue
@@ -5,7 +5,27 @@ import Navigation from "../components/Navigation.vue";
 
 <template>
   <Navigation />
-  <h1>Configurator</h1>
+  <main class="configurator__main">
+    <h1 class="configurator__h1">Configurator</h1>
+    <div class="configurator__donut">
+      <h1>Configurator</h1>
+    </div>
+      <div class="configurator__form">
+        <input type="text" name="name" required />
+        <label for="name" class="configurator__label--wrapper">
+          <span class="configurator__text">Name</span>
+        </label>
+      </div>
+      <div class="configurator__form">
+        <input type="email" name="email" autocomplete="on" required />
+        <label for="email" class="configurator__label--wrapper">
+          <span class="configurator__text">Email</span>
+        </label>
+      </div>
+      <form class="configurator__button" action="" method="post">
+        <button class="hero__btn">Verstuur je donut!</button>
+    </form>
+  </main>
   <Footer />
 </template>
 
