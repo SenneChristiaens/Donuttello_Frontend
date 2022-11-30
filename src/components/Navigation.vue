0a2a79a76b4eab03e5d50818e3b64b6b5266d9a6
Senne <>
2022-11-30 22:00:06 +0100

    feat: add icon for login button & for password change

--- src/components/Navigation.vue
+++ src/components/Navigation.vue
@@ -1,6 +1,18 @@
 <script setup>
 import { computed } from "vue";
 
+/* import the fontawesome core */
+import { library } from "@fortawesome/fontawesome-svg-core";
+
+/* import font awesome icon component */
+import { FontAwesomeIcon } from "@fortawesome/vue-fontawesome";
+
+/* import specific icons */
+import { faUser, faRightFromBracket } from "@fortawesome/free-solid-svg-icons";
+
+/* add icons to the library */
+library.add(faUser, faRightFromBracket);
+
 function logout() {
   localStorage.removeItem("token");
   localStorage.removeItem("nickname");
@@ -27,11 +39,17 @@ const isAdmin = computed(() => {
       <router-link to="/"><a class="nav__item">Home</a></router-link>
       <router-link to="/configurator"><a class="nav__item">Configurator</a></router-link>
       <router-link to="/gallery"><a class="nav__item">Gallerij</a></router-link>
-      <router-link to="/login" v-if="isLoggedOut"><a class="nav__item">Login</a></router-link>
-      <router-link to="/login" v-if="isLoggedIn"><a class="nav__item" @click="logout">Logout</a></router-link>
+      <router-link to="/login" v-if="isLoggedOut"><a class="nav__item"><font-awesome-icon class="icon" icon="fa-solid fa-user" /></a></router-link>
       <router-link to="/admin" v-if="isAdmin"><a class="nav__item">Admin</a></router-link>
+      <router-link to="/changepassword" v-if="isLoggedIn"><a class="nav__item"><font-awesome-icon class="icon" icon="fa-solid fa-user" /></a></router-link>
+      <router-link to="/login" v-if="isLoggedIn"><a class="nav__item" @click="logout"><font-awesome-icon class="icon" icon="fa-solid fa-right-from-bracket"></font-awesome-icon></a></router-link>
     </div>
   </nav>
 </template>
 
-<style></style>
+<style scoped>
+.icon {
+  padding-top: 2.5px;
+  color: #f7f249;
+}
+</style>
