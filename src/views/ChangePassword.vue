ed7b15b265da43d4458e49140b5495867d91ebbd
Senne <>
2022-12-01 22:53:54 +0100

    feat: change password function

--- src/views/ChangePassword.vue
+++ src/views/ChangePassword.vue
@@ -3,10 +3,35 @@ import Navigation from "../components/Navigation.vue";
 import Footerello from "../components/Footerello.vue";
 import { ref } from "vue";
 
-let message = ref(false);
-let password = ref("");
-let password2 = ref("");
+let message = ref("");
+let oldPassword = ref("");
+let newPassword = ref("");
+let token = localStorage.getItem("token");
 
+function changePassword() {
+  fetch(
+    "https://donuttello-backend-5chz.onrender.com/api/v1/admins/changepassword",
+    {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+        Authorization: `Bearer ${token}`,
+      },
+      body: JSON.stringify({
+        oldPassword: oldPassword.value,
+        newPassword: newPassword.value,
+      }),
+    }
+  )
+    .then((response) => response.json())
+    .then((data) => {
+      if (data.status === "success") {
+        message.value = "Je wachtwoord is gewijzigd!";
+      } else {
+        message.value = "Er is iets misgegaan, probeer het opnieuw.";
+      }
+    });
+}
 </script>
 
 <template>
@@ -14,9 +39,14 @@ let password2 = ref("");
   <main class="login">
     <div class="login__box">
       <h1 class="login__h1">Wijzig wachtwoord</h1>
-      <form @submit.prevent="updatePassword">
+      <form @submit.prevent="changePassword">
         <div class="login__form">
-          <input type="password" name="password" v-model="password" required />
+          <input
+            type="password"
+            name="password"
+            v-model="oldPassword"
+            required
+          />
           <label for="password" class="login__label--wrapper">
             <span class="login__text">Oud wachtwoord</span>
           </label>
@@ -26,15 +56,15 @@ let password2 = ref("");
           <input
             type="password"
             name="password2"
-            v-model="password2"
+            v-model="newPassword"
             required
           />
-          <label for="password2" class="login__label--wrapper">
+          <label for="newPassword" class="login__label--wrapper">
             <span class="login__text">Nieuw wachtwoord</span>
           </label>
         </div>
         <p class="message message--bad" v-if="message">
-          Wachtwoorden komen niet overeen.
+          {{ message }}
         </p>
         <button type="submit" class="button--hero">
           Verander mijn wachtwoord
