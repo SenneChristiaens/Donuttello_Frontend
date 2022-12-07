01adf322792eb9aac8c42483aedaf2930ca8f8fd
Senne <>
2022-12-07 11:40:34 +0100

    fix: change name of admin to gallery and update footer

--- src/components/Footerello.vue
+++ src/components/Footerello.vue
@@ -4,6 +4,10 @@ import { computed } from "vue";
 const isAdmin = computed(() => {
   return localStorage.getItem("token") !== null;
 });
+
+const isLoggedOut = computed(() => {
+  return localStorage.getItem("token") === null;
+});
 </script>
 
 <template>
@@ -11,7 +15,8 @@ const isAdmin = computed(() => {
     <img class="footer__img" src="/src/assets/logo.png" alt="logo" />
     <router-link to="/"><a class="nav__item">Home</a></router-link>
     <router-link to="/configurator"><a class="nav__item">Configurator</a></router-link>
-    <router-link to="/admin" v-if="isAdmin"><a class="nav__item">Admin</a></router-link>
+    <router-link to="/admin" v-if="isAdmin"><a class="nav__item">Gallerij</a></router-link>
+    <router-link to="/login" v-if="isLoggedOut"><a class="nav__item">Login</a></router-link>
     <p class="footer__p">© 2022 reserved by Best Donuts gcv</p>
   </footer>
 </template>
--- src/components/Navigation.vue
+++ src/components/Navigation.vue
@@ -39,7 +39,7 @@ const isAdmin = computed(() => {
       <router-link to="/"><a class="nav__item">Home</a></router-link>
       <router-link to="/configurator"><a class="nav__item">Configurator</a></router-link>
       <router-link to="/login" v-if="isLoggedOut"><a class="nav__item"><font-awesome-icon class="icon" icon="fa-solid fa-user" /></a></router-link>
-      <router-link to="/admin" v-if="isAdmin"><a class="nav__item">Admin</a></router-link>
+      <router-link to="/admin" v-if="isAdmin"><a class="nav__item">Gallerij</a></router-link>
       <router-link to="/changepassword" v-if="isLoggedIn"><a class="nav__item"><font-awesome-icon class="icon" icon="fa-solid fa-user" /></a></router-link>
       <router-link to="/login" v-if="isLoggedIn"><a class="nav__item" @click="logout"><font-awesome-icon class="icon" icon="fa-solid fa-right-from-bracket"></font-awesome-icon></a></router-link>
     </div>
