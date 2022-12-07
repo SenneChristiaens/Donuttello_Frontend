9da97dbd99011975ba1913d234db685c597887d2
Senne <>
2022-12-07 11:16:57 +0100

    feat: add editable sprinkles to donut

--- src/components/Three.vue
+++ src/components/Three.vue
@@ -2,6 +2,7 @@
 import * as THREE from "three";
 // import { OrbitControls } from "three/examples/jsm/controls/OrbitControls.js";
 import { GLTFLoader } from "three/examples/jsm/loaders/GLTFLoader.js";
+import { OrbitControls } from "three/examples/jsm/controls/OrbitControls.js";
 import { ref, onMounted, onUnmounted } from "vue";
 
 
@@ -20,7 +21,7 @@ document.body.appendChild(renderer.domElement);
 renderer.setClearColor( 0xffffff, 0);
 
 // orbit controls
-// const controls = new OrbitControls(camera, renderer.domElement);
+const controls = new OrbitControls(camera, renderer.domElement);
 
 // ambient light
 const ambientLight = new THREE.AmbientLight( 0xffffff, 0.5 );
@@ -34,7 +35,7 @@ scene.add( directionalLight );
 // import donut gltf
 let donut;
 const gltfLoaderDonut = new GLTFLoader();
-gltfLoaderDonut.load("/src/assets/DonutNickBevers.glb", (gltf) => {
+gltfLoaderDonut.load("/src/assets/Donut.gltf", (gltf) => {
   donut = gltf.scene;
   gltf.scene.scale.set(5, 5, 5);
   gltf.scene.position.set(0, 0, 0);
@@ -57,63 +58,103 @@ animate();
 // change glaze
 
 function lichtroos_glazuur() {
-  donut.children[0].material.color.set(0xF174BA);
+  donut.children[1].material.color.set(0xF174BA);
 }
 
 function roos_glazuur(){
-  donut.children[0].material.color.set(0xE72870);
+  donut.children[1].material.color.set(0xE72870);
 }
 
 function lichtbruin_glazuur() {
-  donut.children[0].material.color.set(0xBB7E52);
+  donut.children[1].material.color.set(0xBB7E52);
 }
 
 function bruin_glazuur() {
-  donut.children[0].material.color.set(0x673F37);
+  donut.children[1].material.color.set(0x673F37);
 }
 
 function oranje_glazuur() {
-  donut.children[0].material.color.set(0xDA9034);
+  donut.children[1].material.color.set(0xDA9034);
 }
 
 function geel_glazuur() {
-  donut.children[0].material.color.set(0xEDBF04);
+  donut.children[1].material.color.set(0xEDBF04);
 }
 
 function green_glazuur() {
-  donut.children[0].material.color.set(0x7D9A59);
+  donut.children[1].material.color.set(0x7D9A59);
 }
 
 function appelblauwzeegroen_glazuur() {
-  donut.children[0].material.color.set(0xBED4C8);
+  donut.children[1].material.color.set(0xBED4C8);
 }
 
 function wit_glazuur() {
-  donut.children[0].material.color.set(0xffffff);
+  donut.children[1].material.color.set(0xffffff);
 }
 
 function zwart_glazuur() {
-  donut.children[0].material.color.set(0x1E0802);
+  donut.children[1].material.color.set(0x1E0802);
 }
 
 // change dough
 
 function lichtbruin_deeg(){
-  donut.children[1].material.color.set(0xffffff);
+  donut.children[0].material.color.set(0x8b4513);
 }
 
 function bruin_deeg(){
-  donut.children[1].material.color.set(0xd3d3d3);
+  donut.children[0].material.color.set(0x5c3317);
 }
 
 function donkerbruin_deeg(){
-  donut.children[1].material.color.set(0xa4a4a4);
+  donut.children[0].material.color.set(0x3e2317);
 }
+
+// change sprinkles
+
+function rood_sprinkles(){
+  donut.children[2].material.color.set(0xff0000);
+}
+
+function geel_sprinkles(){
+  donut.children[2].material.color.set(0xffff00);
+}
+
+function groen_sprinkles(){
+  donut.children[2].material.color.set(0x00ff00);
+}
+
+function blauw_sprinkles(){
+  donut.children[2].material.color.set(0x0000ff);
+}
+
+function paars_sprinkles(){
+  donut.children[2].material.color.set(0x800080);
+}
+
+function wit_sprinkles(){
+  donut.children[2].material.color.set(0xffffff);
+}
+
+function bruin_sprinkles(){
+  donut.children[2].material.color.set(0x8b4513);
+}
+
+
 
 </script>
 
 <template>
   <h2>Kleuren</h2>
+  <h3>Sprinkles</h3>
+  <button @click="rood_sprinkles">rood</button>
+  <button @click="geel_sprinkles">geel</button>
+  <button @click="groen_sprinkles">groen</button>
+  <button @click="blauw_sprinkles">blauw</button>
+  <button @click="paars_sprinkles">paars</button>
+  <button @click="wit_sprinkles">wit</button>
+  <button @click="bruin_sprinkles">bruin</button>
   <h3>Glazuur</h3>
   <button @click="lichtroos_glazuur">lichtroos</button>
   <button @click="roos_glazuur">roos</button>
