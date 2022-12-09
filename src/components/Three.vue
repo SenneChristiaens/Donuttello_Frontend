6b758319f835b547c18c0a33a959ba4f072bb8b5
RobinVanOverloop <Robinvo@outlook.com>
2022-12-09 16:02:44 +0100

    feat: change camera aspect ratio on resize

--- src/components/Three.vue
+++ src/components/Three.vue
@@ -5,7 +5,6 @@ import { GLTFLoader } from "three/examples/jsm/loaders/GLTFLoader.js";
 import { OrbitControls } from "three/examples/jsm/controls/OrbitControls.js";
 import { ref, onMounted, onUnmounted } from "vue";
 
-
 const scene = new THREE.Scene();
 const camera = new THREE.PerspectiveCamera(
   75,
@@ -14,25 +13,42 @@ const camera = new THREE.PerspectiveCamera(
   1000
 );
 
+// log width and height of canvas
+console.log(window.innerWidth);
+console.log(window.innerHeight);
+
+
 const renderer = new THREE.WebGLRenderer();
-renderer.setSize(window.innerWidth, window.innerHeight);
 onMounted(() => {
-  document.querySelector(".configurator__glaze").appendChild(renderer.domElement);
+  // renderer.setSize(document.querySelector(".configurator__glaze").clientWidths, document.querySelector(".configurator__glaze").clientHeight);
+  renderer.setSize(window.innerWidth/2, window.innerHeight/2);
+  document
+    .querySelector(".configurator__glaze")
+    .appendChild(renderer.domElement);
 });
 
-renderer.setClearColor( 0xffffff, 0);
+
+// change camera aspect ratio on resize
+window.addEventListener("resize", () => {
+  camera.aspect = window.innerWidth / window.innerHeight;
+  camera.updateProjectionMatrix();
+  renderer.setSize(window.innerWidth/2, window.innerHeight/2);
+});
+
+
+renderer.setClearColor(0xffffff, 0);
 
 // orbit controls
-const controls = new OrbitControls(camera, renderer.domElement);
+//const controls = new OrbitControls(camera, renderer.domElement);
 
 // ambient light
-const ambientLight = new THREE.AmbientLight( 0xffffff, 0.5 );
-scene.add( ambientLight );
+const ambientLight = new THREE.AmbientLight(0xffffff, 0.5);
+scene.add(ambientLight);
 
 // directional light
-const directionalLight = new THREE.DirectionalLight( 0xffffff, 1 );
-directionalLight.position.set( 30, 20, 10);
-scene.add( directionalLight );
+const directionalLight = new THREE.DirectionalLight(0xffffff, 1);
+directionalLight.position.set(30, 20, 10);
+scene.add(directionalLight);
 
 // import donut gltf
 let donut;
@@ -44,7 +60,7 @@ gltfLoaderDonut.load("/src/assets/Donut.gltf", (gltf) => {
   scene.add(donut);
 });
 
-camera.position.z = 3;
+camera.position.z = 0.5;
 
 function animate() {
   requestAnimationFrame(animate);
@@ -60,35 +76,35 @@ animate();
 // change glaze
 
 function lichtroos_glazuur() {
-  donut.children[1].material.color.set(0xF174BA);
+  donut.children[1].material.color.set(0xf174ba);
 }
 
-function roos_glazuur(){
-  donut.children[1].material.color.set(0xE72870);
+function roos_glazuur() {
+  donut.children[1].material.color.set(0xe72870);
 }
 
 function lichtbruin_glazuur() {
-  donut.children[1].material.color.set(0xBB7E52);
+  donut.children[1].material.color.set(0xbb7e52);
 }
 
 function bruin_glazuur() {
-  donut.children[1].material.color.set(0x673F37);
+  donut.children[1].material.color.set(0x673f37);
 }
 
 function oranje_glazuur() {
-  donut.children[1].material.color.set(0xDA9034);
+  donut.children[1].material.color.set(0xda9034);
 }
 
 function geel_glazuur() {
-  donut.children[1].material.color.set(0xEDBF04);
+  donut.children[1].material.color.set(0xedbf04);
 }
 
 function green_glazuur() {
-  donut.children[1].material.color.set(0x7D9A59);
+  donut.children[1].material.color.set(0x7d9a59);
 }
 
 function appelblauwzeegroen_glazuur() {
-  donut.children[1].material.color.set(0xBED4C8);
+  donut.children[1].material.color.set(0xbed4c8);
 }
 
 function wit_glazuur() {
@@ -96,82 +112,81 @@ function wit_glazuur() {
 }
 
 function zwart_glazuur() {
-  donut.children[1].material.color.set(0x1E0802);
+  donut.children[1].material.color.set(0x1e0802);
 }
 
 // change dough
 
-function lichtbruin_deeg(){
+function lichtbruin_deeg() {
   donut.children[0].material.color.set(0x8b4513);
 }
 
-function bruin_deeg(){
+function bruin_deeg() {
   donut.children[0].material.color.set(0x5c3317);
 }
 
-function donkerbruin_deeg(){
+function donkerbruin_deeg() {
   donut.children[0].material.color.set(0x3e2317);
 }
 
 // change sprinkles
 
-function rood_sprinkles(){
+function rood_sprinkles() {
   donut.children[2].material.color.set(0xff0000);
 }
 
-function geel_sprinkles(){
+function geel_sprinkles() {
   donut.children[2].material.color.set(0xffff00);
 }
 
-function groen_sprinkles(){
+function groen_sprinkles() {
   donut.children[2].material.color.set(0x00ff00);
 }
 
-function blauw_sprinkles(){
+function blauw_sprinkles() {
   donut.children[2].material.color.set(0x0000ff);
 }
 
-function paars_sprinkles(){
+function paars_sprinkles() {
   donut.children[2].material.color.set(0x800080);
 }
 
-function wit_sprinkles(){
+function wit_sprinkles() {
   donut.children[2].material.color.set(0xffffff);
 }
 
-function bruin_sprinkles(){
+function bruin_sprinkles() {
   donut.children[2].material.color.set(0x8b4513);
 }
-
-
-
 </script>
 
 <template>
-  <h2>Kleuren</h2>
-  <h3>Sprinkles</h3>
-  <button @click="rood_sprinkles">rood</button>
-  <button @click="geel_sprinkles">geel</button>
-  <button @click="groen_sprinkles">groen</button>
-  <button @click="blauw_sprinkles">blauw</button>
-  <button @click="paars_sprinkles">paars</button>
-  <button @click="wit_sprinkles">wit</button>
-  <button @click="bruin_sprinkles">bruin</button>
-  <h3>Glazuur</h3>
-  <button @click="lichtroos_glazuur">lichtroos</button>
-  <button @click="roos_glazuur">roos</button>
-  <button @click="lichtbruin_glazuur">lichtbruin</button>
-  <button @click="bruin_glazuur">bruin</button>
-  <button @click="oranje_glazuur">oranje</button>
-  <button @click="geel_glazuur">geel</button>
-  <button @click="green_glazuur">groen</button>
-  <button @click="appelblauwzeegroen_glazuur">appelblauwzeegroen</button>
-  <button @click="wit_glazuur">wit</button>
-  <button @click="zwart_glazuur">zwart</button>
-  <h3>Deeg</h3>
-  <button @click="lichtbruin_deeg">lichtbruin</button>
-  <button @click="bruin_deeg">bruin</button>
-  <button @click="donkerbruin_deeg">donkerbruin</button>
+  <div class="configurator__details">
+    <h2>Kleuren</h2>
+    <h3>Sprinkles</h3>
+    <button @click="rood_sprinkles">rood</button>
+    <button @click="geel_sprinkles">geel</button>
+    <button @click="groen_sprinkles">groen</button>
+    <button @click="blauw_sprinkles">blauw</button>
+    <button @click="paars_sprinkles">paars</button>
+    <button @click="wit_sprinkles">wit</button>
+    <button @click="bruin_sprinkles">bruin</button>
+    <h3>Glazuur</h3>
+    <button @click="lichtroos_glazuur">lichtroos</button>
+    <button @click="roos_glazuur">roos</button>
+    <button @click="lichtbruin_glazuur">lichtbruin</button>
+    <button @click="bruin_glazuur">bruin</button>
+    <button @click="oranje_glazuur">oranje</button>
+    <button @click="geel_glazuur">geel</button>
+    <button @click="green_glazuur">groen</button>
+    <button @click="appelblauwzeegroen_glazuur">appelblauwzeegroen</button>
+    <button @click="wit_glazuur">wit</button>
+    <button @click="zwart_glazuur">zwart</button>
+    <h3>Deeg</h3>
+    <button @click="lichtbruin_deeg">lichtbruin</button>
+    <button @click="bruin_deeg">bruin</button>
+    <button @click="donkerbruin_deeg">donkerbruin</button>
+  </div>
 </template>
 
 <style></style>
